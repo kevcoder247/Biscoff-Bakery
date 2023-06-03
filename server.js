54c3354facf00151243c999c5dbba47a54f920a0
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-06-02 21:56:22 -0400

    wip: basic servies dependencies added, database, routes and listener

---
+++ server.js
@@ -0,0 +1,31 @@
+// ==========================================
+//               DEPENDENCIES
+// ==========================================
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// ==========================================
+//               DATABASE
+// ==========================================
+const bakedGoods = require('./models/bakedgoods.js');
+
+// ==========================================
+//               ROUTES
+// ==========================================
+// Index route
+app.get('/bakedgoods/', (req, res) => {
+  res.send(bakedGoods);
+});
+
+//Show route
+app.get('/backedgoods/:id', (req, res) => {
+  res.send(backedGoods[req.params.id]);
+})
+
+// ==========================================
+//               LISTENERS
+// ==========================================
+app.listen(PORT, () => {
+  console.log(`Biscoff bakery app is listening on port ${PORT}`)
+})
